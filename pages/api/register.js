a3733e86635276e47d187588ccd9c1ca5d88b162
Yotham <yothamsage@gmail.com>
2024-01-30 06:30:40 -0500

    switched to cookies might switch back depending on localhost

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,5 +1,3 @@
-// pages/api/register.js
-
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../../models/User';
@@ -7,7 +5,7 @@ import dbConnect from '../../utils/dbConnect';
 
 export default async function handler(req, res) {
     await dbConnect();
-    console.log("Here")
+
     if (req.method === 'POST') {
         try {
             const existingUser = await User.findOne({ username: req.body.username });
@@ -22,12 +20,14 @@ export default async function handler(req, res) {
             });
             await user.save();
 
-            console.log("here2 before token")
             const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
                 expiresIn: '24h'
-            
             });
-            res.status(201).json({ message: 'User registered successfully', user, token });
+
+            // Set token in HTTP-only cookie
+            res.setHeader('Set-Cookie', `token=${token}; HttpOnly; Path=/; Max-Age=86400`); // 86400 seconds = 24 hours
+
+            res.status(201).json({ message: 'User registered successfully', user });
         } catch (error) {
             console.error('Registration Error:', error);
             res.status(400).json({ message: 'Error registering user', error: error.message });
