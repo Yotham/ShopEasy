58f07c63fc6b2a8d593a2d18ff516fe01e181d2d
ifoster01 <ifoster41901@gmail.com>
2024-02-11 19:55:13 -0500

    Update LoginForm.js

--- app/components/LoginForm.js
+++ app/components/LoginForm.js
@@ -69,7 +69,7 @@ function LoginForm({ setCurrentUser, setLoginModalOpen }) {
                             type="password"
                             name="password"
                             id="password"
-                            placeholder="••••••••"
+                            placeholder="••••••••••••••"
                             class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             required
                         />
