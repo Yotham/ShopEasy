ba9d1c29b342b017e8d63dd1f792c54d1b3a27e2
ifoster01 <ifoster41901@gmail.com>
2024-02-11 19:54:55 -0500

    Updated login modal
    Updated the css and some minor logic for the login modal

--- app/components/HomePageComponent.js
+++ app/components/HomePageComponent.js
@@ -25,7 +25,7 @@ function HomePageComponent() {
             <div class="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
                 <div class="mx-auto max-w-2xl text-center">
                     <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Welcome to <i>ShopEasy!</i><br />Start hitting your goals today.</h2>
-                    <p class="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-600">We leverage advanced algorithms and a robust database of foods and ingrdients from your local stores to help you meet you fitness goals.</p>
+                    <p class="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-600">We leverage advanced algorithms and a robust database of foods and ingrdients from your local stores to help you meet your fitness goals.</p>
                     <div class="mt-10 flex items-center justify-center gap-x-6">
                     <button
                     class="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -34,10 +34,14 @@ function HomePageComponent() {
                         Login
                     </button>
                     <button
-                    class="text-sm font-semibold leading-6 text-gray-900 hover:text-gray-500"
-                    onClick={() => setRegModalOpen(true)}
-                    >
-                        Get started <span aria-hidden="true">→</span>
+                        onClick={() => setRegModalOpen(true)}
+                        class="group relative inline-block overflow-hidden rounded px-12 py-3 text-sm font-medium text-slate-800 focus:outline-none focus:ring active:bg-blue-500 active:text-white">
+                        <span class="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-blue-500 transition-all duration-200 group-hover:w-full"></span>
+                        <span class="ease absolute right-0 top-0 h-0 w-0 border-r-2 border-blue-500 transition-all duration-200 group-hover:h-full"></span>
+                        <span class="ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-blue-500 transition-all duration-200 group-hover:w-full"></span>
+                        <span class="ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-blue-500 transition-all duration-200 group-hover:h-full"></span>
+                        Get Started
+                        <span aria-hidden="true">→</span>
                     </button>
                     </div>
                 </div>
--- app/components/LoginForm.js
+++ app/components/LoginForm.js
@@ -6,23 +6,24 @@ import { useAuth } from '../../context/AuthContext';
 function LoginForm({ setCurrentUser, setLoginModalOpen }) {
     const router = useRouter();
     const { login } = useAuth(); // Destructure login from the context
-    const [credentials, setCredentials] = useState({
-        username: '',
-        password: ''
-    });
 
     const modalRef = useRef();
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setCredentials(prevState => ({ ...prevState, [name]: value }));
-    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try{
-            console.log('Submitting credentials:', credentials);
-            await login(credentials);
+            
+            const formData = new FormData(e.target);
+            console.log('Submitting credentials:')
+            console.log('FormData:', formData.email);
+
+            const myCredentials = {
+                username: formData.get('username'),
+                password: formData.get('password')
+            }
+
+            console.log('Submitting credentials:', myCredentials);
+            await login(myCredentials);
             router.push('/generate')
             window.location.reload()
         }catch(error){
@@ -45,31 +46,61 @@ function LoginForm({ setCurrentUser, setLoginModalOpen }) {
     }, [setLoginModalOpen]);
 
     return (
-        <div className="login-modal" ref={modalRef}>
-            <form onSubmit={handleSubmit}>
-                <h2>Login</h2>
-                <label>
-                    Username:
-                    <input
-                        type="text"
-                        name="username"
-                        value={credentials.username}
-                        onChange={handleChange}
-                        required
-                    />
-                </label>
-                <label>
-                    Password:
-                    <input
-                        type="password"
-                        name="password"
-                        value={credentials.password}
-                        onChange={handleChange}
-                        required
-                    />
-                </label>
-                <input type="submit" value="Login" />
-            </form>
+        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700" ref={modalRef}>
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                    Sign in to your account
+                </h1>
+                <form class="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+                    <div>
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                        <input
+                            type="text"
+                            name="username"
+                            id="username"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder=""
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="••••••••"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            required
+                        />
+                    </div>
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-start">
+                            <div class="flex items-center h-5">
+                                <input
+                                    id="remember"
+                                    aria-describedby="remember"
+                                    type="checkbox"
+                                    class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                                    required=""
+                                />
+                            </div>
+                            <div class="ml-3 text-sm">
+                                <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                            </div>
+                        </div>
+                        <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                    </div>
+                    <button 
+                        type="submit"
+                        class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                            Log in
+                    </button>
+                    <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                        Don't have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                    </p>
+                </form>
+            </div>
         </div>
     );
 }
