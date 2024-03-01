1dcc4be61da9f0b05678db802f1b234187c68da2
ifoster01 <ifoster41901@gmail.com>
2024-03-01 17:38:32 -0500

    Update RegistrationForm.js

--- app/components/RegistrationForm.js
+++ app/components/RegistrationForm.js
@@ -8,28 +8,7 @@ function RegistrationForm({ setRegModalOpen }) {
         setLoginModalOpen
     } = useAuth();
 
-    const [userData, setUserData] = useState({
-        username: '',
-        password: '',
-        height: [5, 0],  // Default height set to 5'0"
-        weight: '',
-        gender: 'Male',
-        goal: 'Maintain Weight',
-        age: ''
-    });
     const modalRef = useRef();
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setUserData(prevState => ({ ...prevState, [name]: value }));
-    };
-    
-    const handleHeightChange = (type, value) => {
-        const height = [...userData.height];
-        if (type === "feet") height[0] = parseInt(value, 10);
-        else height[1] = parseInt(value, 10);
-        setUserData(prevState => ({ ...prevState, height }));
-    };
 
     const router = useRouter();
 
@@ -206,7 +185,6 @@ function RegistrationForm({ setRegModalOpen }) {
                     <select
                         name="gender"
                         // value={userData.gender}
-                        onChange={handleChange}
                         className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                         required
                     >
@@ -221,7 +199,6 @@ function RegistrationForm({ setRegModalOpen }) {
                     <select
                         name="goal"
                         // value={userData.gender}
-                        onChange={handleChange}
                         className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                         required
                     >
@@ -267,91 +244,6 @@ function RegistrationForm({ setRegModalOpen }) {
                 </p>
             </div>
         </div>
-        // <div className="registration-modal" ref={modalRef}>
-        //     <form onSubmit={handleSubmit}>
-        //         <h2>Register</h2>
-        //                 <label>
-        //                     Username:
-        //                     <input
-        //                         type="text"
-        //                         name="username"
-        //                         value={userData.username}
-        //                         onChange={handleChange}
-        //                         required
-        //                     />
-        //                 </label>
-
-        //                 <label>
-        //                     Password:
-        //                     <input
-        //                         type="password"
-        //                         name="password"
-        //                         value={userData.password}
-        //                         onChange={handleChange}
-        //                         required
-        //                     />
-        //                 </label>
-        //                 <label>
-        //                     Age:
-        //                     <input
-        //                         type="number"
-        //                         name="age"
-        //                         value={userData.age}
-        //                         onChange={handleChange}
-        //                         required
-        //                     />
-        //                 </label>
-        //                 <label>
-        //                     Height:
-        //                     <input 
-        //                         type="number" 
-        //                         name="heightFeet"
-        //                         value={userData.height ? userData.height[0] : ''}
-        //                         onChange={e => handleHeightChange('feet', e.target.value)}
-        //                         placeholder="Feet"
-        //                     />
-        //                     <input 
-        //                         type="number" 
-        //                         name="heightInches"
-        //                         value={userData.height ? userData.height[1] : ''}
-        //                         onChange={e => handleHeightChange('inches', e.target.value)}
-        //                         placeholder="Inches"
-        //                     />
-        //                 </label>
-
-        //                 <label>
-        //                     Weight (in lbs):
-        //                     <input
-        //                         type="number"
-        //                         name="weight"
-        //                         value={userData.weight}
-        //                         onChange={handleChange}
-        //                     />
-        //                 </label>
-
-        //                 <label>
-        //                     Gender:
-        //                     <select name="gender" value={userData.gender} onChange={handleChange}>
-        //                         <option value="">Select...</option>
-        //                         <option value="Male">Male</option>
-        //                         <option value="Female">Female</option>
-        //                         <option value="Other">Other</option>
-        //                     </select>
-        //                 </label>
-
-        //                 <label>
-        //                     Goal:
-        //                     <select name="goal" value={userData.goal} onChange={handleChange}>
-                                // <option value="">Select...</option>
-                                // <option value="Lose Weight">Lose Weight</option>
-                                // <option value="Gain Weight">Gain Weight</option>
-                                // <option value="Maintain Weight">Maintain Weight</option>
-        //                     </select>
-        //                 </label>
-
-        //                 <input type="submit" value="Register" />
-        //             </form>
-        // </div>
     );
 }
 
