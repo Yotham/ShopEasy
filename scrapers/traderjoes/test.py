61727f72676b426c9bff9a478bf6ea737adb1976
Noah Pedroso <n.pedroso@me.com>
2024-04-09 17:24:38 -0400

    All ingredeints retreived

--- scrapers/traderjoes/test.py
+++ scrapers/traderjoes/test.py
@@ -11,55 +11,34 @@ driver = webdriver.Firefox()
 
 # Open a webpage
 base_url = "https://www.traderjoes.com"
-url = urljoin(base_url, "/home/recipes")
-driver.get(url)
-
-# # Find the search input element
-# search_input = driver.find_element(By.NAME, "q")
-
-# # Enter a search query
-# search_input.send_keys("OpenAI")
-
-# # Submit the search query
-# search_input.send_keys(Keys.RETURN)
-
-# # Wait for search results to load
-# WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "search")))
-classes_dict = {
-    "RecipeCard": "Link_link__1AZfr RecipeGridCard_recipe__1Wo__ RecipeGridCard_recipe__tan__12uua",
-}
-
-# Parse the page source with Beautiful Soup
-soup = BeautifulSoup(driver.page_source, "html.parser")
-
-recipe_links = soup.select('div.RecipesGrid_recipesGrid__results__CGnEX a')
-print(recipe_links)
-# Iterate over each <a> element to extract sub-elements
-for link in recipe_links:
-    # Extracting attributes and text from each <a> element
-    href = link['href']
-    img_src = link.select_one('div.RecipeGridCard_recipe__img__1hv4j img')['srcoriginal']
-    categories = link.select_one('p.RecipeGridCard_recipe__categories__3b5AM').text
-    title = link.select_one('h3.RecipeGridCard_recipe__title__3-8S-').text
-
-    # Print or process the extracted data as needed
-    print("Recipe Title:", title)
-    print("Category:", categories)
-    print("Link:", href)
-    print("Image Source:", img_src)
-    print()
-
-# Wait for the previous loop to finish executing
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.RecipesGrid_recipesGrid__results__CGnEX a')))
-print(recipe_links)
+url = urljoin(base_url, "/home/recipes/")
 
 
-driver.get(urljoin(base_url, recipe_links[0]['href']))
+driver.get(urljoin(url, 'instantly-elevated-brownies'))
+# Wait for the content to load
+try:
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".ProductCard_card_verticalCard__1qP8Z"))
+    )
+except:
+    pass
+
 
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-# Find all li elements within the specified class
-li_elements = soup.find('ul', class_='IngredientsList_ingredientsList__1LoAJ').find_all('li')
+
+# Find the ul element with the specified class
+ul_element = soup.find("ul", class_="IngredientsList_ingredientsList__1LoAJ")
+
+# Get the text of each li element within the ul element
+if ul_element:
+    li_elements = ul_element.find_all("li")
+    for li in li_elements:
+        print(li.text)
+        
+# # Find all li elements within the specified class
+# print(soup.select('#_home_recipes_instantly-elevated-brownies_jcr_content_root_body_section_recipe_info_recipe_ingredients > div > div > ul'))
+# li_elements = soup.find('ul', class_='IngredientsList_ingredientsList__1LoAJ').find_all('li')
 
 # Print the text of each li element
 for li in li_elements:
