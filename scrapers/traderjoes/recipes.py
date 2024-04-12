d1381412a21c4158123e2b576945b77f468b72cb
Noah Pedroso <n.pedroso@me.com>
2024-04-12 17:18:36 -0400

    Added pagination

--- scrapers/traderjoes/recipes.py
+++ scrapers/traderjoes/recipes.py
@@ -14,68 +14,63 @@ base_url = "https://www.traderjoes.com"
 url = urljoin(base_url, "/home/recipes")
 driver.get(url)
 
-# # Find the search input element
-# search_input = driver.find_element(By.NAME, "q")
+while True:
+    # Wait for recipes to load
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.RecipesGrid_recipesGrid__results__CGnEX a')))
 
-# # Enter a search query
-# search_input.send_keys("OpenAI")
+    # Parse the page source with Beautiful Soup
+    soup = BeautifulSoup(driver.page_source, "html.parser")
 
-# # Submit the search query
-# search_input.send_keys(Keys.RETURN)
+    recipe_links = soup.select('div.RecipesGrid_recipesGrid__results__CGnEX a')
+    # Iterate over each <a> element to extract sub-elements
+    for link in recipe_links:
+        # Extracting attributes and text from each <a> element
+        href = link['href']
+        img_src = link.select_one('div.RecipeGridCard_recipe__img__1hv4j img')['srcoriginal']
+        categories = link.select_one('p.RecipeGridCard_recipe__categories__3b5AM').text
+        title = link.select_one('h3.RecipeGridCard_recipe__title__3-8S-').text
 
-# # Wait for search results to load
-# WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "search")))
-classes_dict = {
-    "RecipeCard": "Link_link__1AZfr RecipeGridCard_recipe__1Wo__ RecipeGridCard_recipe__tan__12uua",
-}
-WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.RecipesGrid_recipesGrid__results__CGnEX a')))
+        # Print or process the extracted data as needed
+        print("Recipe Title:", title)
+        print("Category:", categories)
+        print("Link:", href)
+        print("Image Source:", img_src)
+        print()
 
-# Parse the page source with Beautiful Soup
-soup = BeautifulSoup(driver.page_source, "html.parser")
+    if len(recipe_links) > 0:
+        print("Found", len(recipe_links), "recipes")
 
-recipe_links = soup.select('div.RecipesGrid_recipesGrid__results__CGnEX a')
-# Iterate over each <a> element to extract sub-elements
-for link in recipe_links:
-    # Extracting attributes and text from each <a> element
-    href = link['href']
-    img_src = link.select_one('div.RecipeGridCard_recipe__img__1hv4j img')['srcoriginal']
-    categories = link.select_one('p.RecipeGridCard_recipe__categories__3b5AM').text
-    title = link.select_one('h3.RecipeGridCard_recipe__title__3-8S-').text
+    for i in range(len(recipe_links)):
+        driver.get(urljoin(base_url, recipe_links[i]['href']))
+        # Wait for the content to load
+        try:
+            WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, ".ProductCard_card_verticalCard__1qP8Z"))
+            )
+        except:
+            pass
 
-    # Print or process the extracted data as needed
-    print("Recipe Title:", title)
-    print("Category:", categories)
-    print("Link:", href)
-    print("Image Source:", img_src)
-    print()
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-if len(recipe_links) > 0:
-    print("Found", len(recipe_links), "recipes")
+        # Find all li elements within the specified class
+        li_elements = soup.find('ul', class_='IngredientsList_ingredientsList__1LoAJ').find_all('li')
 
-for i in range(len(recipe_links)):
-    driver.get(urljoin(base_url, recipe_links[i]['href']))
-    # Wait for the content to load
-    try:
-        WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, ".ProductCard_card_verticalCard__1qP8Z"))
-        )
-    except:
-        pass
+        # Print the text of each li element
+        for li in li_elements:
+            print(li.text)
+        
+        print()
+    
+    
+    # Check if there is a next page button
+    next_button = driver.find_element(By.CSS_SELECTOR, 'button.Pagination_pagination__arrow_side_right__9YUGr')
+    if next_button:
+        next_button.click()  # Click the next page button
+        # Wait for the next page to load
+        WebDriverWait(driver, 20).until(EC.staleness_of(link))
+    else:
+        break  # Exit the loop if there are no more pages
 
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-    # Find all li elements within the specified class
-    li_elements = soup.find('ul', class_='IngredientsList_ingredientsList__1LoAJ').find_all('li')
-
-    # Print the text of each li element
-    for li in li_elements:
-        print(li.text)
-# # Find all search result titles
-# search_results = soup.find_all("a", class_=classes_dict["RecipeCard"])
-
-# # Print out search result titles
-# for result in search_results:
-#     print(result.text)
     
 driver.quit()
 
